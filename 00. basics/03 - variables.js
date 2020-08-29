8bbec906da9d10a36621557fcf529da7c13adeed
Jaleel <jeremikress@gmail.com>
2020-08-29 06:25:19 +0200

    variables

---
+++ 00. basics/03 - variables.js
@@ -0,0 +1,14 @@
+let petName = "Popeye"
+// It's illegal to define more than one once
+// let petName = "Spot"
+
+/*We're not defining a new variable, we're just a new value
+to the existing variable */
+petName = "Spot"
+
+console.log(petName)
+
+//2. There are rules related to variables names
+/* - variable has to start with letter, underscore or dollar sign
+ - Can't use exclamation mark
+ - can't be reserved keywords */
