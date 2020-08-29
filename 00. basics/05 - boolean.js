3df5af4e2cce9e38a5aef3887440c38878e04a64
Jaleel <jeremikress@gmail.com>
2020-08-29 06:57:51 +0200

    booleans

---
+++ 00. basics/05 - boolean.js
@@ -0,0 +1,28 @@
+let temp = 31
+// === - equality operator
+let isFreezing = temp === 31
+
+console.log(isFreezing);
+// !== - inequality operator
+
+let space = 99
+let isNotFull = space !== 100
+
+console.log(isNotFull)
+
+// x < y - x is less than y 
+// x > y - x is greater than y
+
+let grade = 45
+let isPass = grade > 50
+
+console.log(isPass);
+
+//Challenge area. 
+
+let age = 52
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
