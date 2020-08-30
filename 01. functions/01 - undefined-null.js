727f95f45ec690c8de7c07c7874d0d503541fe22
Jaleel <jeremikress@gmail.com>
2020-08-30 14:16:49 +0200

    undefined-null

---
+++ 01. functions/01 - undefined-null.js
@@ -0,0 +1,22 @@
+// Undefined for variable
+let name
+
+console.log(name)
+
+// Undefined for function arguments
+// Undefined as function return default
+
+let square = function(num) {
+    console.log(num);
+}
+
+let result = square()
+console.log(result)
+
+// Null as assigned value
+
+let age = 27
+
+age = null 
+
+console.log(age)
